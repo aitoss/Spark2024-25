350eda48e3d43c3a78617dda90ebbe3666f8474e
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-08-08 19:22:12 +0530

    Added/Timeline

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".smooth-scroll"),
-  smooth: true,
-});
+// const locoScroll = new LocomotiveScroll({
+//   el: document.querySelector(".smooth-scroll"),
+//   smooth: true,
+// });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+// locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".smooth-scroll", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".smooth-scroll").style.transform
-    ? "transform"
-    : "fixed",
+// // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+// ScrollTrigger.scrollerProxy(".smooth-scroll", {
+//   scrollTop(value) {
+//     return arguments.length
+//       ? locoScroll.scrollTo(value, 0, 0)
+//       : locoScroll.scroll.instance.scroll.y;
+//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//   getBoundingClientRect() {
+//     return {
+//       top: 0,
+//       left: 0,
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     };
+//   },
+//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//   pinType: document.querySelector(".smooth-scroll").style.transform
+//     ? "transform"
+//     : "fixed",
+// });
+
+// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+// ScrollTrigger.refresh();
+
+const dayIndicator = document.getElementById("day-indicator");
+const cards = document.querySelectorAll(".idv-card");
+
+cards.forEach((card, index) => {
+  ScrollTrigger.create({
+    trigger: card,
+    start: "top center",
+    end: "bottom center",
+    onEnter: () => updateDayIndicator(index + 1),
+    onEnterBack: () => updateDayIndicator(index + 1),
+  });
 });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+function updateDayIndicator(day) {
+  dayIndicator.textContent = `DAY ${day}`;
+}
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+// gsap.to(dayIndicator, {
+//   scrollTrigger: {
+//     trigger: ".cards-container",
+//     start: "top top",
+//     end: "bottom top",
+//     pin: true,
+//     pinSpacing: false,
+//   },
+// });
+
+// gsap.to("#slider-navigation", {
+//   scrollTrigger: {
+//     trigger: ".cards-container",
+//     start: "top top",
+//     end: "bottom top",
+//     pin: true,
+//     pinSpacing: false
+//   }
+// });
